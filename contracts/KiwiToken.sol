9f7c05ba67ec27c7931f6612d1496bc90794d405
Maximiliaan van Dijk <max@maxilibrium.com>
2021-08-28 01:42:34 -0500

    Added token swapper

--- contracts/KiwiToken.sol
+++ contracts/KiwiToken.sol
@@ -9,9 +9,16 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract KiwiToken is Context, AccessControlEnumerable, ERC20Burnable, ERC20Permit, ERC20VotesComp {
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract KiwiToken is Context, AccessControlEnumerable, ERC20Burnable, ERC20Permit, ERC20VotesComp, ReentrancyGuard {
+  using SafeERC20 for IERC20;
+
   address public constant initialLiquidityProvider = 0xedEA1d58Ad20E30Ff03a660A773757E1c8209D13;
   address public constant devWallet = 0xa15C8F207c1DD6790e231E5ba2e415b163007441;
+  IERC20 public constant oldKiwi = IERC20(0xD664b8d8624750a4f945fe9efb1438e9e021cd56);
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
   
   bool public init = true;
@@ -46,5 +53,11 @@ contract KiwiToken is Context, AccessControlEnumerable, ERC20Burnable, ERC20Perm
   function mint(address to, uint256 amount) public virtual {
     require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
     _mint(to, amount);
+  }
+
+  function swapOldKiwi (uint swapAmount) external nonReentrant {    
+    oldKiwi.safeTransferFrom(msg.sender, 0x000000000000000000000000000000000000dEaD, swapAmount);
+
+    _mint(msg.sender, swapAmount);
   }
 }
